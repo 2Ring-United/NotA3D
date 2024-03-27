7647ef193480ab663e353e7f2d67135585581d56
JustMax <k.szwedkowicz01@gmail.com>
2024-03-27 16:30:50 +0100

    Adds simple enemy spawning

--- Assets/Scripts/Managers/EnemySpawnManager.cs
+++ Assets/Scripts/Managers/EnemySpawnManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,11 +6,48 @@ public class EnemySpawnManager : MonoBehaviour
 {
     public static UnityEvent OnEnemyDeath;
 
+    
+    public static EnemySpawnManager _instance;
+    public static EnemySpawnManager Instance { get { return _instance; } private set { } }
 
-    public static EnemySpawnManager Instance;
 
-    public List<Transform> SpawnPoints;
+    [SerializeField] List<GameObject> P_Enemies;
 
+    [SerializeField] List<Transform> spawnPoints;
+
+    [SerializeField] List<GameObject> spawnedEnemies;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        spawnedEnemies = new List<GameObject>();
+        SpawnEnemies();
+    }
+
+    public void SpawnEnemies()
+    {
+        foreach(Transform t in spawnPoints)
+        {
+            int rand = Random.Range(0, 101);
+            if(rand <= 50 ) 
+            {
+                rand = Random.Range(0, P_Enemies.Count);
+                spawnedEnemies.Add(Instantiate(P_Enemies[rand], t));
+            }
+            
+        }
+    }
 
     public void InvokeEnemyDeathEvent()
     {
